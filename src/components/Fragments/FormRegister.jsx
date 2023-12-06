5070b3e5e2d124fefb51b50d2d863a0a38a3c61f
marselino kartawijaya <90839452+Acelino12@users.noreply.github.com>
2023-12-06 10:02:36 +0700

    Update judul

--- src/components/Fragments/FormRegister.jsx
+++ src/components/Fragments/FormRegister.jsx
@@ -28,8 +28,40 @@ const FormRegister = () =>{
                 placeholder="******" 
                 name="confir password"/>
                 <br/>
+            <select>
+                <option value="pria">pria</option>
+                <option value="wanita">wanita</option>
+            </select>
+                <br />
+            <textarea name="text" id="text" cols="30" rows="10"></textarea>
+            <br />
+
+            <input type="date" name="" id="" />
+
+            <input type="radio" name="asd" id="asd" />
+
+            <input type="color" name="" id="" />
+
+            <input type="datetime-local" name="" id="" />
+
+            <br /><br />
+
+            <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/J_WuWM454cE?si=5iD6ATJe8KdrLP_6&amp;start=60" 
+            title="YouTube video player"
+             
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+            </iframe>
+
+            <br /><br /><br />
+
+
+
             <Button variant="bg-blue-600">Register</Button>
       </form>
+
     );
 };
 
